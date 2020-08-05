52287a15646e8628bb1c56783903774d03bd6b4d
Jonathan Neufeld <support@extollit.com>
2020-08-05 00:06:23 -0700

    Boost computational power of low scheduling priority

--- src/main/java/com/extollit/gaming/ai/path/SchedulingPriority.java
+++ src/main/java/com/extollit/gaming/ai/path/SchedulingPriority.java
@@ -18,10 +18,10 @@ public enum SchedulingPriority {
     /**
      * Indicates low-priority scheduling, entities with engines configured for this rating complete path-finding later.
      * This is initialized with default values:
-     *  - 3 initial compute iterations
-     *  - 2 subsequent compute iterations
+     *  - 6 initial compute iterations
+     *  - 4 subsequent compute iterations
      */
-    low     (3, 2);
+    low     (6, 4);
 
     protected int initComputeIterations, periodicComputeIterations;
 
